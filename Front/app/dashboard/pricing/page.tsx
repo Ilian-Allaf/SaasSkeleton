6797947a07605a8fd78bb00216a17309c4734ea4
Faruukko <ilian.allaf@outlook.fr>
2024-05-14 22:56:53 +0200

    debug pricing

--- Front/app/dashboard/pricing/page.tsx
+++ Front/app/dashboard/pricing/page.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
 import Skeleton from './skeleton'
 import Stripe from 'stripe';
-import { cookies } from 'next/headers'
+// import { cookies } from 'next/headers'
 
 import { authOptions } from "@/auth/[...nextauth]";
 import { setupGraphQLClient } from "@/lib/gqlclient";
@@ -10,9 +9,8 @@ import { getServerSession } from "next-auth"
 
 
 export default async function page() {
-  const cookieStore = cookies();
-  const languageCode = cookieStore.get('i18next')?.value as string;
-  console.log("languageCode" + languageCode)
+  // const cookieStore = cookies();
+  // const languageCode = cookieStore.get('i18next')?.value as string;
 
   const session = await getServerSession(authOptions)
   const gqlClient = await setupGraphQLClient();
@@ -22,27 +20,17 @@ export default async function page() {
       };
   };
   const userPlan = await gqlClient!.request( GetUserDocument, { id: session.user.id } );
-  console.log("userPlan" + userPlan)
-  const subscribtionPlans = await gqlClient!.request( GetSubscribtionPlansDocument, { languageCode: languageCode } );
-  console.log("subscribtionPlans" + subscribtionPlans)
+  const subscribtionPlans = await gqlClient!.request( GetSubscribtionPlansDocument, { languageCode: 'en' } );
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, { apiVersion: '2023-10-16' });
 
   const priceMap = {};
-
-  if (!subscribtionPlans || !subscribtionPlans.subscribtion_plan) {
-    // Handle the case where subscribtionPlans is null or subscribtion_plan is not available
-    console.error('Subscription plans not available');
-}
-else {
   for (const subscribtionPlan of subscribtionPlans.subscribtion_plan) {
     const price = await stripe.prices.retrieve(subscribtionPlan.id);
     console.log("price" + price)
     priceMap[subscribtionPlan.id] = price.unit_amount;
   }
-}
 
 
-  console.log("priceMap" + priceMap)
   return (
     <>
       <Skeleton subscribtionPlans={subscribtionPlans} priceMap={priceMap} userPlan={userPlan}/>
