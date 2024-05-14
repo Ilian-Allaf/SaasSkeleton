8c3cd469d96ec2192ad0e1311d7a6d08718cc66f
Faruukko <ilian.allaf@outlook.fr>
2024-05-14 23:18:14 +0200

    debug pricing

--- Front/app/dashboard/pricing/page.tsx
+++ Front/app/dashboard/pricing/page.tsx
@@ -1,3 +1,5 @@
+"use server"
+
 import Skeleton from './skeleton'
 import Stripe from 'stripe';
 import { cookies } from 'next/headers'
@@ -19,22 +21,21 @@ export default async function page() {
           error: 'Not Authenticated',
       };
   };
-  // const userPlan = await gqlClient!.request( GetUserDocument, { id: session.user.id } );
-  // const subscribtionPlans = await gqlClient!.request( GetSubscribtionPlansDocument, { languageCode: 'en' } );
-  // const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, { apiVersion: '2023-10-16' });
+  const userPlan = await gqlClient!.request( GetUserDocument, { id: session.user.id } );
+  const subscribtionPlans = await gqlClient!.request( GetSubscribtionPlansDocument, { languageCode: 'en' } );
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, { apiVersion: '2023-10-16' });
 
-  // const priceMap = {};
-  // for (const subscribtionPlan of subscribtionPlans.subscribtion_plan) {
-  //   const price = await stripe.prices.retrieve(subscribtionPlan.id);
-  //   console.log("price" + price)
-  //   priceMap[subscribtionPlan.id] = price.unit_amount;
-  // }
+  const priceMap = {};
+  for (const subscribtionPlan of subscribtionPlans.subscribtion_plan) {
+    const price = await stripe.prices.retrieve(subscribtionPlan.id);
+    console.log("price" + price)
+    priceMap[subscribtionPlan.id] = price.unit_amount;
+  }
 
 
   return (
     <>
-      this is the pricing page
-      {/* <Skeleton subscribtionPlans={subscribtionPlans} priceMap={priceMap} userPlan={userPlan}/> */}
+      <Skeleton subscribtionPlans={subscribtionPlans} priceMap={priceMap} userPlan={userPlan}/>
     </>
   )
 };
