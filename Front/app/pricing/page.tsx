a46425f96711b6915f37be915496f58e4fdbc2fe
Faruukko <ilian.allaf@outlook.fr>
2024-05-22 20:42:36 +0200

    Fixed i18 issue

--- Front/app/pricing/page.tsx
+++ Front/app/pricing/page.tsx
@@ -2,8 +2,7 @@
 
 import Skeleton from './skeleton'
 import Stripe from 'stripe';
-import { cookies } from 'next/headers'
-
+import { useTranslation } from '@/i18n/index'
 import { authOptions } from "@/auth/[...nextauth]";
 import { setupUnauthenticatedGraphQLClient } from "@/lib/unauthenticatedGqlClient";
 import { setupGraphQLClient } from "@/lib/gqlClient";
@@ -11,9 +10,7 @@ import { GetSubscriptionPlansDocument, GetUserDocument, GetUserQuery } from "@/s
 import { getServerSession } from "next-auth"
 
 export default async function page() {
-  const cookieStore = cookies();
-  const languageCode = cookieStore.get('i18next')?.value as string;
-
+  const { i18n } = await useTranslation('settings')
   const session = await getServerSession(authOptions)
   const gqlClient = session ? await setupGraphQLClient(): await setupUnauthenticatedGraphQLClient();
   let userPlan: GetUserQuery = {};
@@ -21,7 +18,7 @@ export default async function page() {
     userPlan = await gqlClient!.request( GetUserDocument, { id: session.user.id } );
   };
 
-  const subscribtionPlans = await gqlClient!.request( GetSubscriptionPlansDocument, { languageCode: languageCode } );
+  const subscribtionPlans = await gqlClient!.request( GetSubscriptionPlansDocument, { languageCode: i18n.language } );
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, { apiVersion: '2023-10-16' });
 
   const priceMap = {};
@@ -29,7 +26,6 @@ export default async function page() {
     const price = await stripe.prices.retrieve(subscribtionPlan.id);
     priceMap[subscribtionPlan.id] = price.unit_amount;
   }
-
 
   return (
     <>
