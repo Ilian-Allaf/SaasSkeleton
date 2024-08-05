ea022b4374d50ea04e40252500819da4d775e14c
Faruukko <ilian.allaf@outlook.fr>
2024-08-05 21:13:18 +0200

    merged feedback migration files

--- Hasura/hasura-back/migrations/default/1722364263021_create_unsubscription_feedback_tables/up.sql
+++ Hasura/hasura-back/migrations/default/1722364263021_create_unsubscription_feedback_tables/up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "public"."unsubscription_feedback" (
-    "id" UUID NOT NULL,
+    "id" UUID DEFAULT gen_random_uuid() NOT NULL,
     "feedback" TEXT NOT NULL,
     CONSTRAINT "unsubscription_feedback_pkey" PRIMARY KEY ("id")
 );
--- Hasura/hasura-back/migrations/default/1722365994141_alter_table_public_unsubscription_feedback_alter_column_id/down.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "public"."unsubscription_feedback" ALTER COLUMN "id" drop default;
