d7d80f593456e84d364d5c432cdf2984eed42248
Faruukko <ilian.allaf@outlook.fr>
2024-08-05 21:16:06 +0200

    Added mock users and mock teams

---
+++ Hasura/hasura-back/migrations/default/1700658830954_insert_into_public_team/up.sql
@@ -0,0 +1,5 @@
+INSERT INTO "public"."team"("id", "name") VALUES (E'c28dfa84-37bf-4b5a-ba31-02d8542e2c2d', E'ilianTeam');
+INSERT INTO "public"."team"("id", "name") VALUES (E'5244d980-afd3-4b58-990c-93e12f056a91', E'romanTeam');
+INSERT INTO "public"."team"("id", "name") VALUES (E'f50a9e7d-cae1-438c-9ac6-14046c14cb75', E'victorTeam');
+INSERT INTO "public"."team"("id", "name") VALUES (E'8cdd436e-1be7-4b04-96f8-604fb4cb561b', E'lukasTeam');
+INSERT INTO "public"."team"("id", "name") VALUES (E'9b0f7ebc-fb64-4c52-8787-2e0b4714ee7b', E'tanguyTeam');
